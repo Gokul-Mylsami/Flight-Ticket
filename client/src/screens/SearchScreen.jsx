a1510e0e85d77593258f835c9e9f310e6127e25b
Gokul-Mylsami <gokulmylsami@gmail.com>
2023-05-06 23:02:57 +0530

    Flight Card

--- client/src/screens/SearchScreen.jsx
+++ client/src/screens/SearchScreen.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import "../css/screens/SearchScreen.css";
 import SearchBar from "../components/Searchbar";
+import FlightDetailsCard from "../components/FlightDetailsCard";
 
 const SearchScreen = () => {
   return (
     <div className="search-screen-section">
       <Navbar />
       <SearchBar />
-      <div></div>
+      <div>
+        <FlightDetailsCard />
+      </div>
     </div>
   );
 };
